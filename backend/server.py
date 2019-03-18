4727798df95ff33892040b18640d25a649319436
imruahmed <im.ahmed97@gmail.com>
2019-03-17 18:11:03 -0700

    Change parameter to database url

--- backend/server.py
+++ backend/server.py
@@ -11,7 +11,7 @@ migrate = Migrate()
 def create_app():
     app = Flask(__name__)
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['SQLALCHEMY_DATABASE_URI']
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
 
