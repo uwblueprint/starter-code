64f6a6e0a8905009040e64f57f1f46d63e5d7220
Ruxuan Li <ruxuan.li@gmail.com>
2019-03-31 11:37:22 -0700

    Added endpoint to retrieve data from db

--- backend/routes.py
+++ backend/routes.py
@@ -1,8 +1,12 @@
 from flask import Blueprint
+from flask import jsonify
+
+import backend.db.client as db_client
 
 blueprint = Blueprint('api', __name__)
 
 
-@blueprint.route('/')
-def index():
-    return "hello, world!"
+@blueprint.route('/classrooms')
+def classrooms():
+    return jsonify(db_client.get_all_classroom_data())
+    
