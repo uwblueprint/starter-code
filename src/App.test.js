776e42fb968a38272bd5f7d889f2b56ffb44f7b7
suzyng83209 <suzyn.one@gmail.com>
2019-04-07 11:14:25 -0700

    initial component with a lot of test files and added new modules for usefulness

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+/**
+ * The most basic React test file will attempt to render the component without crashing.
+ * Please include more unit tests which test component generation like in the example.
+ */
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
