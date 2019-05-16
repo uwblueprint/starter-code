b488cc4aef17996f002e881fbcc8877a6ef6bdbd
suzyng83209 <suzyn.one@gmail.com>
2019-05-16 15:10:02 -0400

    added as a click to title; will move elsewhere

--- src/components/Title.js
+++ src/components/Title.js
@@ -3,10 +3,16 @@ import PropTypes from 'prop-types';
 
 import './Title.scss';
 
+const handleClick = () => { 
+  fetch('/recycling-data')
+    .then(res => res.json())
+    .then(console.log);
+};
+
 const Title = ({ teamName }) => {
   return (
     <div className="title">
-      <h1 className="container">
+      <h1 className="container" onClick={handleClick}>
         {teamName.charAt(0).toUpperCase() + teamName.slice(1)} Elementary's Recycling Race Progress Tracker:
       </h1>
     </div>
@@ -17,8 +23,8 @@ export default Title;
 
 Title.propTypes = {
   teamName: PropTypes.string.isRequired
-}
+};
 
 Title.defaultProps = {
   teamName: ""
-}
+};
