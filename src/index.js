940400862104d088314147ee75cfa220dbf892a3
suzyng83209 <suzyn.one@gmail.com>
2019-05-17 11:08:40 -0400

    add firebase support

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
+import * as firebase from 'firebase';
+
 import configureStore from './store';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 import './index.css';
+
+// fill in firebaseConfig with your app's firebase config.
+// Instructions here: https://firebase.google.com/docs/web/setup
+// Then uncomment the block below.
+//
+// const firebaseConfig = {}
+
+// firebase.initializeApp(firebaseConfig);
 
 /**
  * While App.js is the root of your React App, index.js is the root of your frontend.
