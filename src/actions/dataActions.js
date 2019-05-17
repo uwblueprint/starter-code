b00303bc16f7a25b41c9e0dc3ba1052aec08f038
suzyng83209 <suzyn.one@gmail.com>
2019-05-16 23:01:05 -0400

    add comment

--- src/actions/dataActions.js
+++ src/actions/dataActions.js
@@ -1,5 +1,9 @@
 import { LOAD_DATA, CLEAR_DATA } from '../types';
 
+/**
+ * Actions should be as light weight as possible. No asynchronous processes should be started in actions.
+ * This is why the data fetching was done prior to loading the data to the global store.
+ */
 export const loadData = data => ({
   type: LOAD_DATA,
   payload: data
