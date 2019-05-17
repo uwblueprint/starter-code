f1a5508b7e54ea9e17f1a9cc7517aa32531140b5
suzyng83209 <suzyn.one@gmail.com>
2019-05-17 11:08:40 -0400

    add firebase support

--- src/components/Display.js
+++ src/components/Display.js
@@ -3,6 +3,8 @@
  */
 import React from 'react';
 import BasicTable from './BasicTable';
+import firebase from 'firebase';
+import 'firebase/firestore';
 
 import './Display.scss';
 
@@ -10,7 +12,8 @@ class Display extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: []
+      data: [],
+      dataFirebase: {}
     };
   }
 
@@ -23,12 +26,31 @@ class Display extends React.Component {
       });
   }
 
+  fetchDataFirebase = () => {
+    const fdb = firebase.firestore();
+    fdb
+      .collection("recycled_material")
+      .get()
+      .then(snapshot => {
+        const data = {};
+        snapshot
+          .forEach(entry => {
+            data[entry.id] = entry.data();
+          })
+        this.setState({ dataFirebase: data })
+      })
+      .catch(e => {
+        console.error("something went wrong", e)
+      })
+  }
+
   /**
    * This is a React Component Lifecycle method. 
    * It will fire when the component has been mounted onto the DOM tree.
    */
   componentDidMount() {
     this.fetchData();
+    // this.fetchDataFirebase();
   }
 
   render() {
@@ -38,6 +60,12 @@ class Display extends React.Component {
         <BasicTable data={this.state.data} />
         <h2>Global Data Handling</h2>
         <BasicTable  data={this.props.storeData} />
+        <h2>Firebase Data (json)</h2>
+        {
+          Object.entries(this.state.dataFirebase).length === 0
+            ? "**Firebase not set up**"
+            : JSON.stringify(this.state.dataFirebase, null, 2)
+        }
       </div>
     )
   }
