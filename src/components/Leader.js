5bed24509f6833217bb8c1a4f79b12ec41c7771a
Aaron Yim <aaronyim@outlook.com>
2019-05-18 15:09:44 -0400

    Added sumCollected function to Leader.js for calculating score by teacher

--- src/components/Leader.js
+++ src/components/Leader.js
@@ -3,6 +3,19 @@ import PropTypes from 'prop-types';
 
 import './Leader.scss';
 
+const sumCollected = (object = {}) => {
+    const teachers = Object.keys(object);
+    let sum = {};
+    for (const [ key, value ] of Object.entries(object)) {
+            let collected = 0;
+            for (const [key, value] of Object.entries(value)){
+                collected += Number(value);
+            }
+            sum[key] = collected;
+    }
+    return sum;
+}
+
 export default Leader;
 
 Leader.defaultProps = {
